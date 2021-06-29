3a7cf4626da902c919d0c168ad5a246ee22a622e
zhangxiangxi <zhangxiangxi@xdf.cn>
2021-06-29 19:36:48 +0800

    keep-alive

--- 源码-vue2/src/core/components/keep-alive.js
+++ 源码-vue2/src/core/components/keep-alive.js
@@ -15,6 +15,8 @@ function getComponentName (opts: ?VNodeComponentOptions): ?string {
   return opts && (opts.Ctor.options.name || opts.tag)
 }
 
+/* 检测name是否匹配 */
+
 function matches (pattern: string | RegExp | Array<string>, name: string): boolean {
   if (Array.isArray(pattern)) {
     return pattern.indexOf(name) > -1
@@ -27,19 +29,22 @@ function matches (pattern: string | RegExp | Array<string>, name: string): boole
   return false
 }
 
+// 修补 cache
 function pruneCache (keepAliveInstance: any, filter: Function) {
   const { cache, keys, _vnode } = keepAliveInstance
   for (const key in cache) {
+     // 尝试获取 cache中的vnode
     const entry: ?CacheEntry = cache[key]
     if (entry) {
       const name: ?string = entry.name
-      if (name && !filter(name)) {
+      if (name && !filter(name)) { // 重新筛选组件
         pruneCacheEntry(cache, key, keys, _vnode)
       }
     }
   }
 }
 
+/* 销毁vnode对应的组件实例（Vue实例） */
 function pruneCacheEntry (
   cache: CacheEntryMap,
   key: string,
@@ -58,7 +63,7 @@ const patternTypes: Array<Function> = [String, RegExp, Array]
 
 export default {
   name: 'keep-alive',
-  abstract: true,
+  abstract: true, // ，若 abstract 为 true，则表示组件是一个抽象组件，不会被渲染到真实DOM中，也不会出现在父组件链中
 
   props: {
     include: patternTypes,
@@ -78,6 +83,7 @@ export default {
         }
         keys.push(keyToCache)
         // prune oldest entry
+        // 如果配置了 max 并且缓存的长度超过了 this.max 则从缓存中删除第一个
         if (this.max && keys.length > parseInt(this.max)) {
           pruneCacheEntry(cache, keys[0], keys, this._vnode)
         }
@@ -87,6 +93,7 @@ export default {
   },
 
   created () {
+     /* 缓存对象 */
     this.cache = Object.create(null)
     this.keys = []
   },
@@ -99,6 +106,7 @@ export default {
 
   mounted () {
     this.cacheVNode()
+    // 通过 watch 来监听 include 和 exclude，在其改变时调用 pruneCache 以修改 cache 缓存中的缓存数据。
     this.$watch('include', val => {
       pruneCache(this, name => matches(val, name))
     })
@@ -111,14 +119,23 @@ export default {
     this.cacheVNode()
   },
 
+// 通过getFirstComponentChild获取第一个子组件，获取该组件的name（存在组件名则直接使用组件名，否则会使用tag）
+// 将name通过include与exclude属性进行匹配，匹配不成功（说明不需要缓存）则直接返回vnode
+// 匹配成功则尝试获取缓存的组件实例
+// 若没有缓存该组件，则缓存该组件
+// 缓存超过最大值会删掉第一个缓存
+
   render () {
+     /* 得到slot插槽中的第一个组件 */
     const slot = this.$slots.default
     const vnode: VNode = getFirstComponentChild(slot)
     const componentOptions: ?VNodeComponentOptions = vnode && vnode.componentOptions
     if (componentOptions) {
       // check pattern
+      // 获取组件名称，优先获取组件的name字段，否则是组件的tag
       const name: ?string = getComponentName(componentOptions)
       const { include, exclude } = this
+      // 不需要缓存，则返回 vnode
       if (
         // not included
         (include && (!name || !matches(include, name))) ||
@@ -135,16 +152,18 @@ export default {
         ? componentOptions.Ctor.cid + (componentOptions.tag ? `::${componentOptions.tag}` : '')
         : vnode.key
       if (cache[key]) {
+        // 有缓存则取缓存的组件实例
         vnode.componentInstance = cache[key].componentInstance
         // make current key freshest
         remove(keys, key)
         keys.push(key)
       } else {
         // delay setting the cache until update
+        // 无缓存则创建缓存
         this.vnodeToCache = vnode
         this.keyToCache = key
       }
-
+       // keepAlive标记
       vnode.data.keepAlive = true
     }
     return vnode || (slot && slot[0])
